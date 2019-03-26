a4ed2c906753b7680e928a8b1fb7aba664240c6c
Krunal Astik <krunalastik7@gmail.com>
2019-03-26 17:24:20 +0530

    Directory Structure Created and home page designed

---
+++ index.php
@@ -0,0 +1,471 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Digihire &mdash; Finding the right opportunities </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="stylesheet" href="assets/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="assets/fonts/flaticon/font/flaticon.css">
+
+
+
+    <link rel="stylesheet" href="assets/css/aos.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Stylesheet used by developer for custom styling-->
+    <link rel="stylesheet" href="assets/css/customstyle.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar py-3" role="banner">
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-11 col-xl-2">
+            <h1 class="mb-0"><a href="index.php" class="text-white h2 mb-0">Logo</a></h1>
+          </div>
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.html">For Company</a></li>
+                <li><a href="industries.html">Help</a></li>
+                <li><a href="blog.html">Faq</a></li>
+                <li><a href="contact.html">Opportunities</a></li>
+                <li><a href="contact.html">Login</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+          </div>
+
+        </div>
+      </div>
+      
+    </header>
+
+  
+
+    <div class="site-blocks-cover overlay" style="background-image: url(assets/images/intern.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+            
+
+            <h1 class="text-white font-weight-light mb-5 text-uppercase font-weight-bold">Opportunities will not wait long.</h1>
+            <p><a href="#" class="btn btn-primary py-3 px-5 text-white">Sign Up!</a></p>
+
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+        <div class="align-items-center no-gutters align-items-stretch mt-5">
+          <div class="feature-1 pricing h-100 text-center">
+            <table class="text-white table table-bordered">
+            <tr>
+                <th>ORGANIZATION</th>
+              </tr>
+              <tr>
+                <td>NGO</td>
+              </tr>              
+              <tr>
+                <th>DURATION</th>
+              </tr>
+              <tr>
+                <td>3-6 Months</td>
+              </tr>
+              <tr>
+                <th>COMPENSATION</th>
+              </tr>
+              <tr>
+                <td>3000+</td>
+              </tr>
+              <tr>
+                <td><i><a href='login.php' class="text-white">Apply Now</a></i></td>
+              </tr>
+              <tr>
+                <th><a href='ngo_details.php' class="text-white">Read More...</a></th>
+              </tr>
+            </table>
+          </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+        <div class="align-items-center no-gutters align-items-stretch mt-5">
+          <div class="feature-1 pricing h-100 text-center">
+            <table class="text-white table table-bordered">
+            <tr>
+                <th>ORGANIZATION</th>
+              </tr>
+              <tr>
+                <td>MNC</td>
+              </tr>              
+              <tr>
+                <th>DURATION</th>
+              </tr>
+              <tr>
+                <td>6-15 Months</td>
+              </tr>
+              <tr>
+                <th>COMPENSATION</th>
+              </tr>
+              <tr>
+                <td>12000+</td>
+              </tr>
+              <tr>
+                <td><i><a href='login.php' class="text-white">Apply Now</a></i></td>
+              </tr>
+              <tr>
+                <th><a href='mnc_details.php' class="text-white">Read More...</a></th>
+              </tr>
+            </table>
+          </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+          <div class="align-items-center no-gutters align-items-stretch mt-5">
+          <div class="feature-1 pricing h-100 text-center">
+            <table class="text-white table table-bordered">
+              <tr>
+                <th>ORGANIZATION</th>
+              </tr>
+              <tr>
+                <td>Startups</td>
+              </tr>  
+              <tr>
+                <th>DURATION</th>
+              </tr>
+              <tr>
+                <td>6-12 Months</td>
+              </tr>
+              <tr>
+                <th>COMPENSATION</th>
+              </tr>
+              <tr>
+                <td>10000+</td>
+              </tr>
+              <tr>
+                <td><i><a href='login.php' class="text-white">Apply Now</a></i></td>
+              </tr>
+              <tr>
+                <th><a href='startup_details.php' class="text-white">Read More...</a></th>
+              </tr>
+            </table>
+          </div>
+          </div>
+          </div>
+        
+                        
+        </div>
+      </div>
+    </div>
+    <div class="site-section mt-5 border-top border-bottom" style="background-color:#FB8C00;color:white;">
+      <div class="container">
+      <div class="row">
+        <div class="slide-one-item home-slider owl-carousel" >
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <p><b>Front-End Developer</b></p>
+              </figure>
+              <blockquote>
+                  <span class="float-md-left float-lg-left">Company: Digihire</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-right float-lg-right">Location: Shrinagar</span>
+                  <br />
+                  <span class="float-md-right float-lg-right">Durations: 6 Months</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Background: Angular Js, Node.Js Symentic UI</span>
+              </blockquote>
+            </div>
+          </div>
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <p><b>Backend-End Developer</b></p>
+              </figure>
+              <blockquote>
+                  <span class="float-md-right float-lg-right">Company: CodeBinary</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Location: Kanyakumari</span>
+                  <br>
+                  <span class="float-md-right float-lg-right">Durations: 4 Months</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Background: Node.Js, Amazon Aurora</span>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <p><b>Graphics Designer</b></p>
+              </figure>
+              <blockquote>
+                  <span class="float-md-right float-lg-right">Company: Kalpanaa</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Location: Guhar Moti</span>
+                  <br />
+                  <span class="float-md-right float-lg-right">Durations: 8 Months</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Background: Adobe Photoshop, Illustrator, Indesign</span>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <p><b>Business Development Executive</b></p>
+              </figure>
+              <blockquote>
+                  <span class="float-md-right float-lg-right">Company: Digihire</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Location: Kibithu</span>
+                  <br />
+                  <span class="float-md-right float-lg-right">Durations: 3 Months</span>
+                  <br class="d-md-none d-lg-none">
+                  <span class="float-md-left float-lg-left">Background: MBA,BBA, Sales Experience</span>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section border-bottom">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Testimonials</h2>
+          </div>
+        </div>
+        <div class="slide-one-item home-slider owl-carousel">
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="assets/images/person_3.jpg" alt="Image" class="img-fluid mb-3">
+                <p>John Smith</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="assets/images/person_2.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Christine Aguilar</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="assets/images/person_4.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Robert Spears</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="assets/images/person_5.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Bruce Rogers</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Partner Slider Starts-->
+    <div class="container mt-5 mb-5">
+    <div class="row justify-content-center mb-5">
+     <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Partners</h2>
+     </div>
+    </div>
+  </div>
+    <div class="row mb-5">
+    <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:135px;overflow:hidden;visibility:hidden;">
+        <!-- Loading Screen -->
+        <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
+            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
+        </div>
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:140px;overflow:hidden;">
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/codebinary.png" class="part_pad"/>
+              </a>
+            </div>
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/kalpanaa.jpg" class="part_pad"/>
+              </a>
+              </div>
+            <div>
+              <a href="https://www.sproutingwingsdigitalmarketing.com/" target="_blank">
+                <img data-u="image" src="assets/images/spdm.jpeg" class="part_pad"/>
+              </a>
+            </div>                                    
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/codebinary.png" class="part_pad"/>
+              </a>
+            </div>
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/kalpanaa.jpg" class="part_pad"/>
+              </a>
+              </div>
+            <div>
+              <a href="https://www.sproutingwingsdigitalmarketing.com/" target="_blank">
+                <img data-u="image" src="assets/images/spdm.jpeg" class="part_pad"/>
+              </a>
+            </div>                                    
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/codebinary.png" class="part_pad"/>
+              </a>
+            </div>
+            <div>
+              <a href="http://codebinary.in" target="_blank">
+                <img data-u="image" src="assets/images/kalpanaa.jpg" class="part_pad"/>
+              </a>
+              </div>
+            <div>
+              <a href="https://www.sproutingwingsdigitalmarketing.com/" target="_blank">
+                <img data-u="image" src="assets/images/spdm.jpeg" class="part_pad"/>
+              </a>
+            </div>                                                           
+        </div>
+    </div>
+    </div>
+    </div>
+    <!--Partner Slider Ends-->
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Digihire</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Intro Intro Intro Intro Intro Intro Intro Intro</a></li>
+                  <li><a href="#">Logo</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">About</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Help</a></li>
+                  <li><a href="#">Blogs</a></li>
+                  <li><a href="#">News</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Opportunities</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Volunteer</a></li>
+                  <li><a href="#">Startup</a></li>
+                  <li><a href="#">Talent</a></li>
+                  <li><a href="#">Training</a></li>
+                  <li><a href="#">Courses</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Organization</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Partners</a></li>
+                  <li><a href="#">Volunteer</a></li>
+                  <li><a href="#">Talent</a></li>
+                  <li><a href="#">Startups</a></li>
+                  <li><a href="#">Courses</a></li>
+                </ul>
+              </div>              
+            </div>
+          </div>
+          <div class="col-md-3">
+            <h2 class="footer-heading mb-4">Registered</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Membership</a></li>
+                  <li><a href="#">Join Us</a></li>
+                  <li><a href="#">Countries</a></li>
+                  <li><a href="#">Programs</a></li>
+                </ul>
+              </div>            
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-12">
+            <div class="border-top">
+            <p>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved  by Sprouting Wings and it's associates.
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="assets/js/jquery-ui.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/jquery.stellar.min.js"></script>
+  <script src="assets/js/jquery.countdown.min.js"></script>
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="assets/js/bootstrap-datepicker.min.js"></script>
+  <script src="assets/js/aos.js"></script>
+
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/partner_slider.js"></script>
+  <script type="text/javascript">jssor_1_slider_init();</script>  
+</body>
+</html>
